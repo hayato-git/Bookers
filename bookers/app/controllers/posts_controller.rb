354cb44b3772995ec0a2581f468b07000c585241
岸颯人 <hayatokishi@MacBook-Pro.local>
2019-04-08 12:22:37 +0900

    [Add]投稿機能

---
+++ bookers/app/controllers/posts_controller.rb
@@ -0,0 +1,25 @@
+class PostsController < ApplicationController
+  def show
+  end
+
+  def index
+  end
+
+  def new
+  	@post = Post.new
+  end
+
+  def create
+  	post = Post.new(post_params)
+  	post.save
+  	redirect_to posts_path
+  end
+
+  def edit
+  end
+
+  private
+  def post_params
+  	params.require(:post).permit(:title, :body)
+  end
+end
