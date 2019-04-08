2aca86d17ec3ae071be9d577ea4204d1f97b42d6
岸颯人 <hayatokishi@MacBook-Pro.local>
2019-04-08 18:00:05 +0900

    [Fix]html(new.index)

--- bookers/app/controllers/posts_controller.rb
+++ bookers/app/controllers/posts_controller.rb
@@ -1,20 +1,16 @@
 class PostsController < ApplicationController
-  def show
-    @post = Post.find(params[:id])
+  def new
+    @post = Post.new
+  end
+
+  def create
+    post = Post.new(post_params)
+    post.save
+    redirect_to posts_path(post.id)
   end
 
   def index
     @posts = Post.all
-  end
-
-  def new
-  	@post = Post.new
-  end
-
-  def create
-  	post = Post.new(post_params)
-  	post.save
-  	redirect_to posts_path
   end
 
   def edit
@@ -24,7 +20,7 @@ class PostsController < ApplicationController
   def update
     post = Post.find(params[:id])
     post.update(post_params)
-    redirect_to post_path(post)
+    redirect_to post_path(post.id)
   end
 
   def destroy
@@ -33,8 +29,14 @@ class PostsController < ApplicationController
     redirect_to posts_path
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
+
   private
   def post_params
   	params.require(:post).permit(:title, :body)
   end
+
 end
